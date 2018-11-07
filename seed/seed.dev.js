b7e5680aac762cb9d100e08562e83ea65033d2e8
cadrob <robertcaddock@gmail.com>
2018-11-07 12:59:41 +0000

    Working seed with users and topics insterted

---
+++ seed/seed.dev.js
@@ -0,0 +1,13 @@
+const seedDB = require('./seed');
+const mongoose = require('mongoose');
+const { DB_URL } = require('../config')
+const rawData = require('../seed/testData')
+
+mongoose.connect(DB_URL, { useNewUrlParser: true })
+.then(() => {
+    console.log('connected to database');
+    return seedDB(rawData) //object containing the other 4 objects
+})
+.then(() => mongoose.disconnect());
+
+
