db38ef711c6bfabbf0d1b8cac9f59e8a97eb3bf4
cadrob <robertcaddock@gmail.com>
2018-11-11 21:12:22 +0000

    More changes to config setup

--- seed/run-seed.js
+++ seed/run-seed.js
@@ -1,6 +1,6 @@
 const seedDB = require('./seed');
 const mongoose = require('mongoose');
-const { DB_URL } = process.env.DB_URL || require('../config')
+const DB_URL  = process.env.DB_URL || require('../config')
 const rawData = require('../seed/devData')
 
 mongoose.connect(DB_URL, { useNewUrlParser: true })
